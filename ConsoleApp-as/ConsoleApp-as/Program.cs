e05ab7434fc97a604a0d2a26efc3c55cadfa1eeb
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-09 14:27:00 +0900

    メソッドを追加

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -6,11 +6,27 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-           for (int feet = 1; feet <= 10; feet++)
+            if (args.Length >= 1 && args[0] == "-tom")
             {
-                double meter = FeetToMeter(feet);
-                Console.WriteLine("{0}ft = {1:0.0000} m", feet, meter);
+                for (int feet = 1; feet <= 10; feet++)
+                {
+                    double meter = FeetToMeter(feet);
+                    Console.WriteLine("{0}ft = {1:0.0000} m", feet, meter);
+                }
             }
+            else
+            {
+                for (int meter = 1; meter <= 10; meter++)
+                {
+                    double feet = MeterToFeet(meter);
+                    Console.WriteLine("{0}m = {1:0.0000} ft", meter, feet);
+                }
+            }
+        }
+
+        static double MeterToFeet(int meter)
+        {
+            return meter / 0.3048;
         }
 
         static double FeetToMeter(int feet)
