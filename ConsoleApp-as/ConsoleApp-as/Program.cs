3b41426df0d76239f77fb06af57beb921207cdc8
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-09 10:50:22 +0900

    構造体　DateTime

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -6,51 +6,11 @@ namespace ConsoleApp_as
     {
         static void Main(string[] args)
         {
-            Product karinto = new Product(123, "かりんとう", 180);
-            Product daihuku = new Product(235, "大福もち", 360); //複数のインスタンスも生成できる
+            DateTime date = new DateTime(2016, 8, 20);
+            int year = date.Year;
 
-            int price = karinto.Price;
-            int daihukuPrice = daihuku.Price;
-
-            int karintoTax = karinto.GetTax(); //変数karintoを通じてオブジェクトにアクセスできる
-            int daihukuTax = daihuku.GetTax();
-
-            int daihukuTotal = daihukuPrice + daihukuTax;
-
-            int taxIncluded = karinto.GetPriceIncludingTax();
-
-            Console.WriteLine(karinto);
-            Console.WriteLine(daihuku);
-            Console.WriteLine(daihukuTotal);
-            Console.WriteLine("かりんとうの税率は{0}円です", daihukuTotal);
-            Console.WriteLine(taxIncluded);
-
-        }
-    }
-
-    public class Product
-    {
-        public int Code { get; private set; }
-        public string Name { get; private set; }
-        public int Price { get; private set; }
-        
-
-        //コンストラクタ
-        public Product (int code, string name, int price)
-        {
-            this.Code = code;
-            this.Name = name;
-            this.Price = price;
-        }
-
-        public int GetTax()
-        {
-            return (int)(Price * 0.08);
-        }
-
-        public int GetPriceIncludingTax()
-        {
-            return Price + GetTax();
+            //10日後をもとめる
+            DateTime daysAfter10 = date.AddDays(10);
         }
     }
 }
