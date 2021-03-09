c793a20fb593f68f4d88c23c5adff651863d87b8
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-09 09:31:12 +0900

    クラスの基礎

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -6,7 +6,38 @@ namespace ConsoleApp_as
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Product karinto = new Product(123, "かりんとう", 180);
+
+            int price = karinto.Price;
+
+            int taxIncluded = karinto.GetPriceIncludingTax();
+
+
+        }
+    }
+
+    public class Product
+    {
+        public int Code { get; private set; }
+        public string Name { get; private set; }
+        public int Price { get; private set; }
+
+        //コンストラクタ
+        public Product (int code, string name, int price)
+        {
+            this.Code = code;
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public int GetTax()
+        {
+            return (int)(Price * 0.08);
+        }
+
+        public int GetPriceIncludingTax()
+        {
+            return Price + GetTax();
         }
     }
 }
