41e78aca794aec7d27914179a72deed03d76379a
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-09 09:57:40 +0900

    クラスの呼び出し

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -7,11 +7,23 @@ namespace ConsoleApp_as
         static void Main(string[] args)
         {
             Product karinto = new Product(123, "かりんとう", 180);
+            Product daihuku = new Product(235, "大福もち", 360); //複数のインスタンスも生成できる
 
             int price = karinto.Price;
+            int daihukuPrice = daihuku.Price;
+
+            int karintoTax = karinto.GetTax(); //変数karintoを通じてオブジェクトにアクセスできる
+            int daihukuTax = daihuku.GetTax();
+
+            int daihukuTotal = daihukuPrice + daihukuTax;
 
             int taxIncluded = karinto.GetPriceIncludingTax();
 
+            Console.WriteLine(karinto);
+            Console.WriteLine(daihuku);
+            Console.WriteLine(daihukuTotal);
+            Console.WriteLine("かりんとうの税率は{0}円です", daihukuTotal);
+            Console.WriteLine(taxIncluded);
 
         }
     }
@@ -21,6 +33,7 @@ namespace ConsoleApp_as
         public int Code { get; private set; }
         public string Name { get; private set; }
         public int Price { get; private set; }
+        
 
         //コンストラクタ
         public Product (int code, string name, int price)
