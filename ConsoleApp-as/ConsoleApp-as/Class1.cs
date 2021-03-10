72ece8df2a5421079da6cf366016fb46f45dd4b5
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-10 13:40:54 +0900

    feet,meterの対応表の作成

---
+++ ConsoleApp-as/ConsoleApp-as/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DistanceConverter
+{
+    class FeetConverter
+    {
+        public double FromMeter(double meter)
+        {
+            return meter / 0.3048;
+        }
+
+        public double ToMeter(double feet)
+        {
+            return feet * 0.3048;
+        }
+    }
+}
--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,19 +8,30 @@ namespace DistanceConverter
         {
             if (args.Length >= 1 && args[0] == "-tom")
             {
-                for (int feet = 1; feet <= 10; feet++)
-                {
-                    double meter = FeetToMeter(feet);
-                    Console.WriteLine("{0}ft = {1:0.0000} m", feet, meter);
-                }
+                PrintFeetToMeterList(1, 10);
             }
             else
             {
-                for (int meter = 1; meter <= 10; meter++)
-                {
-                    double feet = MeterToFeet(meter);
-                    Console.WriteLine("{0}m = {1:0.0000} ft", meter, feet);
-                }
+                PrintMeterToFeetList(1, 10);
+            }
+        }
+
+        //メートルからフィートへの対応表を出力
+        static void PrintMeterToFeetList(int start, int stop)
+        {
+            for (int meter = start; meter <= stop; meter++)
+            {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine("{0}m = {1:0.0000} ft", meter, feet);
+            }
+        }
+        //フィートからメートルへの対応表を出力
+        static void PrintFeetToMeterList(int start, int stop)
+        {
+            for (int feet = start; feet <= stop; feet++)
+            {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine("{0}ft = {1:0.0000} m", feet, meter);
             }
         }
 
