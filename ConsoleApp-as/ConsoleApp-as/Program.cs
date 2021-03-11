86c4ae595ec9634040d8c2a21bf2555d15df07dc
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-11 21:43:41 +0900

    演習問題９

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,8 +8,11 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            
-
+            Console.Write("購入金額：");
+            var line = Console.ReadLine();
+            var product = int.Parse(line);
+            var point = product * 0.05;
+            Console.WriteLine($"ポイント5倍day!! {point}pointです");
 
         }
         
