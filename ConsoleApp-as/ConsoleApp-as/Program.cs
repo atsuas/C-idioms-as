c72061c4b07d6a5670cefcb94f29e046fc97ef2d
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-11 21:00:17 +0900

    不要なコードの削除

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,14 +8,7 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            var a = 10;
-            var b = 20;
-            Console.WriteLine($"a={a}, b={b}");
-
-            var change = a;
-            a = b;
-            b = change;
-            Console.WriteLine($"a={a}, b={b}");
+            
 
 
         }
