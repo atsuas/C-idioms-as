053edfb015fcbfb3df8ee21d58e205c11c583f71
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-11 19:09:34 +0900

    演習問題２

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,8 +8,9 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("はじめまして。");
-            Console.WriteLine("私はプログラミングが好きです");
+            var name = "近藤";
+            var age = 19;
+            Console.WriteLine("{0}さんは、{1}歳です", name, age);
 
         }
         
