8fac54ecdc697e0b29f8af56a2a4023af6db0c01
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-11 20:28:26 +0900

    演習問題６

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,14 +8,21 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-           
-            Console.Write("金額を入力してください：");
-            var line = Console.ReadLine();
-            var price = int.Parse(line);
-            var tax = 1.1;
-            var totalPrice = (int)(price * tax);
-            Console.WriteLine("商品価格は{0}円です", totalPrice);
+            var n1 = 100;
+            var n2 = 13;
+            var a1 = n1 + n2; //113
+            var a2 = -n1 / n2; //-7
+            var a3 = (double)n1 / n2; //7.692307692307692
+            var a4 = a2 - n1; //-107
+            var a5 = a3 * -2; //-15.38461538461538
+            var a6 = a4 * 2.0M; //-216
 
+            Console.WriteLine(a1);
+            Console.WriteLine(a2);
+            Console.WriteLine(a3);
+            Console.WriteLine(a4);
+            Console.WriteLine(a5);
+            Console.WriteLine(a6);
         }
         
     }
