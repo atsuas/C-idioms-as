f10d5bd2b7c25846e55283384ecab7d67ae872b7
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-11 20:59:32 +0900

    演習問題８

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,19 +8,15 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            var x = 8;
-            x /= 2;
-            Console.WriteLine(x); //4
+            var a = 10;
+            var b = 20;
+            Console.WriteLine($"a={a}, b={b}");
 
-            var a = 4;
-            var b = 6;
-            a += b * 2;
-            Console.WriteLine(a); //16
+            var change = a;
+            a = b;
+            b = change;
+            Console.WriteLine($"a={a}, b={b}");
 
-            var n = 5;
-            var k = 3;
-            n *= (k + 4);
-            Console.WriteLine(n); //35
 
         }
         
