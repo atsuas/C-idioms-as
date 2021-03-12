7df04ed71d0f4f0f3ec24306322493d9f63f1470
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-12 18:38:32 +0900

    *回数を入力する　練習

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,7 +8,18 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-           
+            Console.Write("*回数を入力してください：");
+            var line = Console.ReadLine();
+            var count = int.Parse(line);
+            for (int n = 0; n < count; n++)
+            {
+                var star = "*";
+                for (int i = 0; i < n; i++)
+                {
+                    star += "*";
+                }
+                Console.WriteLine(star);
+            }
         }
         
     }
