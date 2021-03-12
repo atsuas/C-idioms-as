efacf48f7c08cb787c88844f883519257b7b60c5
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-12 17:18:04 +0900

    不要なソースの削除

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,20 +8,7 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            var num = 0;
-            while (num >= 0)
-            {
-                Console.Write("正の数を入力してください：");
-                var line = Console.ReadLine();
-                num = int.Parse(line);
-
-                if (num < 0)
-                {
-                    Console.WriteLine("処理を抜けます");
-                    break;
-                }
-            }
-            Console.WriteLine($"{num}が入力されました");
+           
         }
         
     }
