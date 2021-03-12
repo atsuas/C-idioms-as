8009376c6333949678c0f15272d037b3d6cba4a7
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-12 17:55:14 +0900

    不要なソースの削除

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,15 +8,7 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            var count = 0;
-            for (int num = 0; num <= 500; num++)
-            {
-                if (((num % 3 == 0) || (num % 7 == 0)) && (num % 2 == 1))
-                {
-                    count++;
-                }
-            }
-            Console.WriteLine(count);
+            
         }
         
     }
