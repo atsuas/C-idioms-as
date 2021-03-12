70cc536ba24204567a6d8a309a093dc9a88934f9
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-12 15:02:30 +0900

    不要なソースの削除

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,31 +8,7 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            var word = Console.ReadLine();
-            var term = "";
-
-            switch (word)
-            {
-                case "API":
-                    term = "Application Programing Interface";
-                    break;
-                case "ROB":
-                    term = "Relational Datebase";
-                    break;
-                case "UI":
-                    term = "User Interface";
-                    break;
-                case "IDE":
-                    term = "Integrated Development Environment";
-                    break;
-                case "HTML":
-                    term = "HyperText Markup Language";
-                    break;
-            }
-            if (term != "")
-            {
-                Console.WriteLine($"{word}の略は{term}です");
-            }
+            
         }
         
     }
