4f7eb482016793ce97cf9812ea37f1a2444d5672
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-12 17:40:07 +0900

    不要なソースの削除

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,18 +8,7 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            var maxline = "";
-            for (int i = 0; i < 5; i++)
-            {
-                Console.Write("文字を入力してください：");
-                var line = Console.ReadLine();
-                var len = line.Length;
-                if (len > maxline.Length)
-                {
-                    maxline = line;
-                }
-            }
-            Console.WriteLine(maxline);
+            
         }
         
     }
