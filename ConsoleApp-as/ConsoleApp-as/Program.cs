78e451d2e14f249baa200e150fcd0cc050be265a
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-12 18:13:01 +0900

    入力された数値を足していく　練習

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,7 +8,19 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            
+            var total = 0.0;
+            while (true)
+            {
+                Console.Write("数値を入力してください：");
+                var line = Console.ReadLine();
+                var num = double.Parse(line);
+                if (num <= 0)
+                {
+                    break;
+                }
+                total += num;
+            }
+            Console.WriteLine(total);
         }
         
     }
