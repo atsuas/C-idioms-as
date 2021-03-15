78e7c1fdba3c2f092427c0f443eb9b39c0b4d0ac
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-15 21:41:09 +0900

    不要なコードの削除

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,49 +8,9 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            var bmicalc = BmiCalculator.GetBmi(180, 90);
-            var body = BmiCalculator.GetBodyType(bmicalc);
-            Console.WriteLine("あなたのBMIは{0}、タイプは{1}です", bmicalc, body);
+
         }
     }
 
-    static class BmiCalculator
-    {
-        public static double GetBmi(double height, double weight)
-        {
-            var meterTall = height / 100.0;
-            var bmi = weight / (meterTall * meterTall);
-            return bmi;
-        }
-
-        public static string GetBodyType(double bmi)
-        {
-            var type = "";
-            if (bmi < 18.5)
-            {
-                type = "瘦せ型";
-            }
-            else if (bmi < 25)
-            {
-                type = "普通体重";
-            }
-            else if (bmi < 30)
-            {
-                type = "肥満1度";
-            }
-            else if (bmi < 35)
-            {
-                type = "肥満2度";
-            }
-            else if (bmi < 40)
-            {
-                type = "肥満3度";
-            }
-            else
-            {
-                type = "肥満4度";
-            }
-            return type;
-        }
-    }
+    
 }
