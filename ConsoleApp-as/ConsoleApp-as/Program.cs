5d17e067a18c58cced2cb5fa0ffa49f734aaad38
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-15 09:16:58 +0900

    クラスの使い方　練習

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,18 +8,45 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            var nums = new int[5];
-            var total = 0;
-            for (int i = 0; i < nums.Length; i++)
+            var book1 = new Book
             {
-                var line = Console.ReadLine();
-                var num = int.Parse(line);
-                total += num;
-            }
-            var average = total / nums.Length;
-            Console.WriteLine(average);
+                Title = "猫の恩返し",
+                Auther = "高橋",
+                Pages = 370,
+                Rate = 4,
+                Memo = "おもしろい"
+            };
+
+            var book2 = new Book
+            {
+                Title = "魚物語",
+                Auther = "小林",
+                Pages = 290,
+                Rate = 5,
+                Memo = "まあまあ"
+            };
+
+            book1.Print();
+            book2.Print();
+            
+            
         }
         
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public string Auther { get; set; }
+        public int Pages { get; set; }
+        public int Rate { get; set; }
+        public string Memo { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"タイトル：{Title}");
+            Console.WriteLine($"著者：{Auther}, ページ数：{Pages},レート：{Rate}, メモ：{Memo}");
+        }
     }
 
 }
