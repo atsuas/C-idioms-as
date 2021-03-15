744cc5abb088baad5984cb95b3b3e0536f6fbbc3
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-16 01:19:30 +0900

    途中

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,37 +8,32 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            var sale = new Sale
-            {
-                ProductName = "もやし",
-                UnitPrice = 150,
-                Quantity = 80
-            };
-            Console.WriteLine($"商品名：{sale.ProductName} 価格：{sale.UnitPrice}円  量：{sale.Quantity}g");
+            
         }
 
-        class Sale
-        {
-            private string _productName;
-            private int _unitPrice;
-            private int _quantity;
-            public string ProductName
-            {
-                get { return _productName; }
-                set { _productName = value; }
-            }
-            public int UnitPrice
-            {
-                get { return _unitPrice; }
-                set { _unitPrice = value; }
-            }
-            public int Quantity
-            {
-                get { return _quantity; }
-                set { _quantity = value; }
-            }
-        }
     }
 
-    
+    static class ArrayUtils
+    {
+        public static int Total(int[] numbers)
+        {
+            var total = 0;
+            foreach (var n in numbers)
+            {
+                total += n;
+            }
+            return total;
+        }
+
+        public static double Average(int[] numbers)
+        {
+            var total = Total(numbers);
+            return (double)total / numbers.Length;
+        }
+
+        public static int GetMin(int[] isPosition)
+        {
+
+        }
+    }
 }
