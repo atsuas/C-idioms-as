99ef55ab227d2745b3e087585e887c5447ad31f9
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-15 22:02:38 +0900

    Math.Minの練習

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,6 +8,12 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
+            var line1 = Console.ReadLine();
+            var line2 = Console.ReadLine();
+            var num1 = int.Parse(line1);
+            var num2 = int.Parse(line2);
+            var min = Math.Min(num1, num2);
+            Console.WriteLine(min);
 
         }
     }
