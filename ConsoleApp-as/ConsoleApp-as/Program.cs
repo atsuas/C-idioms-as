53d60c38713372995353e6a84b010e62bd8118bc
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-15 23:19:36 +0900

    不要なコードの削除

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,25 +8,10 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            var dayday = Date();
-            Console.WriteLine(dayday);
-
-            TheDay();
+            
         }
 
-        public static int Date()
-        {
-            var month = DateTime.DaysInMonth(2021, 2);
-            return month;
-        }
-
-        public static void TheDay()
-        {
-            var line = Console.ReadLine();
-            var days = DateTime.Parse(line);
-            var week = days.ToString("ddd");
-            Console.WriteLine($"{week}");
-        }
+       
     }
 
     
