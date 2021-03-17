9882dedd46cd72858259046965d78473c087ef91
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-17 14:26:21 +0900

    生年月日が月曜だったら表示する　練習

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,8 +8,17 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            
-
+            Console.Write("生年月日を入力してください:");
+            var line = Console.ReadLine();
+            var comment = DateTime.Parse(line);
+            if (comment.DayOfWeek == DayOfWeek.Monday)
+            {
+                Console.WriteLine("月曜日です");
+            }
+            else
+            {
+                Console.WriteLine("月曜日ではありません");
+            }
         }
 
     }
