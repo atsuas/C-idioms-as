8609af80fc962eee19da3ac76a45b39b425d1267
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-17 15:46:55 +0900

    値型と参照型の練習

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -6,11 +6,34 @@ namespace DistanceConverter
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
+            var king = new Card(CardSuit.Spade, 13);
+            Console.WriteLine($"Suit:{king.Suit}, Number:{king.Number}");
+            ChangeToAce(king);
+            Console.WriteLine($"Suit:{king.Suit}, Number:{king.Number}");
         }
 
+        private static void ChangeToAce(Card card)
+        {
+            card = new Card(CardSuit.Club, 1);
+        }
     }
 
+    enum CardSuit
+    {
+        Club, Spade, Heart, Diamond
+    }
+
+    class Card
+    {
+        public CardSuit Suit { get; set; }
+        public int Number { get; set; }
+
+        public Card(CardSuit suit, int number)
+        {
+            Suit = suit;
+            Number = number;
+        }
+    }
 }
