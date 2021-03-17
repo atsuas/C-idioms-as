ab4f083917e88e1baa5afb9a53638a50bbd98192
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-17 18:55:23 +0900

    不要なコードの削除

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,32 +8,9 @@ namespace DistanceConverter
     {
         static void Main()
         {
-            var king = new Card(CardSuit.Spade, 13);
-            Console.WriteLine($"Suit:{king.Suit}, Number:{king.Number}");
-            ChangeToAce(king);
-            Console.WriteLine($"Suit:{king.Suit}, Number:{king.Number}");
+           
         }
 
-        private static void ChangeToAce(Card card)
-        {
-            card = new Card(CardSuit.Club, 1);
-        }
     }
 
-    enum CardSuit
-    {
-        Club, Spade, Heart, Diamond
-    }
-
-    class Card
-    {
-        public CardSuit Suit { get; set; }
-        public int Number { get; set; }
-
-        public Card(CardSuit suit, int number)
-        {
-            Suit = suit;
-            Number = number;
-        }
-    }
 }
