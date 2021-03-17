5090fc99d66c026d14682b9ea41c877f75044b2e
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-17 19:29:39 +0900

    不要なコードの削除

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,32 +8,9 @@ namespace DistanceConverter
     {
         static void Main()
         {
-            var dates = new List<DateTime>
-            {
-                new DateTime(2000,11, 8, 12, 8, 0),
-                new DateTime(2020, 8, 4,23, 6, 0),
-                new DateTime(2010, 4, 5, 8, 1, 0),
-                new DateTime(2018,12,12,10,13, 0),
-                new DateTime(2016, 3,18,15,35, 0),
-                new DateTime(2014, 4, 1,18,46, 0)
-            };
-
-            var count = dates.Count;
-            Console.WriteLine(count);
-
-            foreach (var n in dates)
-            {
-                Console.WriteLine(n.ToString("yyyy年mm月dd日 HH:mm"));
-            }
-
             
-
-
-
         }
 
     }
-
-
 
 }
