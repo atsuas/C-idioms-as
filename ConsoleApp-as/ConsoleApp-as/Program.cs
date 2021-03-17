40881647d09969d10e78212e34dcb205ac2c2974
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-17 14:12:10 +0900

    不要なコードの削除

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,34 +8,10 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            var book = new Book("風の谷のナウシカ", "小林")
-            {
-                Pages = 380,
-                Rate = 5
-            };
-            book.Print();
+            
 
         }
 
     }
 
-    class Book
-    {
-        public Book(string title, string auther)
-        {
-            Title = title;
-            Auther = auther;
-        }
-
-        public string Title { get; set; }
-        public string Auther { get; set; }
-        public int Pages { get; set; }
-        public int Rate { get; set; }
-
-        public void Print()
-        {
-            Console.WriteLine($"タイトル：{Title} 著者：{Auther} ページ数：{Pages} 評価：{Rate}");
-        }
-
-    }
 }
