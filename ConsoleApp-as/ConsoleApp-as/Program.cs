f45bc1a823ed91677987896bde03215498b30ffa
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-17 14:47:20 +0900

    不要なコードの削除

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -8,41 +8,9 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            var person = new Person("小林", "たかし", Gender.Femele);
-            Console.WriteLine($"{person.FirstName} {person.LastName} {person.Gender}");
+            
         }
 
-    }
-
-    enum Gender
-    {
-        Male,
-        Femele
-    }
-
-    class Person
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
-        public Gender Gender { get; set; }
-        public string Fullname
-        {
-            get { return LastName + FirstName; }
-        }
-
-        public Person()
-        {
-            FirstName = "";
-            LastName = "";
-        }
-
-        public Person(string firstName, string lastName, Gender gender)
-        {
-            FirstName = firstName;
-            LastName = lastName;
-            Gender = gender;
-        }
     }
 
 }
