b6c7268aaac17978c480f4f0ecfdb7778ae7fc5d
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-18 14:27:28 +0900

    プロパティの旧式　練習

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -9,9 +9,42 @@ namespace DistanceConverter
     {
         static void Main()
         {
-           
+            var sale = new Sale
+            {
+                UnitPrice = 300,
+                Quantity = 2,
+                ProductName = "たこやき"
+            };
+            Console.WriteLine($"{sale.ProductName} {sale.UnitPrice}円 {sale.Quantity}個");
         }
        
+    }
+
+    class Sale
+    {
+        private int _unitPrice;
+        private int _quantity;
+        private string _productName;
+
+        public int UnitPrice
+        {
+            get { return _unitPrice; }
+            set { _unitPrice = value; }
+        }
+
+        public int Quantity
+        {
+            get { return _quantity; }
+            set { _quantity = value; }
+        }
+
+        public string ProductName
+        {
+            get { return _productName; }
+            set { _productName = value; }
+        }
+
+
     }
 
 }
