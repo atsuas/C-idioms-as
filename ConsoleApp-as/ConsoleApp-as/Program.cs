536fa968f96ac02a94c891106cbdf8e4b44ffa28
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-18 14:54:57 +0900

    不要なコードの削除

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -9,29 +9,9 @@ namespace DistanceConverter
     {
         static void Main()
         {
-            var num = new int[] { 20, -10, 42, 56, 11, -4 };
-            Console.WriteLine(ArrayUtil.GetMin(num, true));
-            Console.WriteLine(ArrayUtil.GetMin(num));
+            
         }
        
     }
-
-    class ArrayUtil
-    {
-        public static int GetMin(int[] number, bool isPosition = false)
-        {
-            var min = int.MaxValue;
-            foreach (var n in number)
-            {
-                if (isPosition && n <= 0)
-                {
-                    continue;
-                }
-                min = Math.Min(min, n);
-            }
-            return min;
-        }
-    }
-
 
 }
