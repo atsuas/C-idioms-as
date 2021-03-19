5a60edcb08d06517e8b7b0b675c4e4a9a30cd77e
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-19 14:49:41 +0900

    不要なコードの削除

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -9,57 +9,9 @@ namespace DistanceConverter
     {
         static void Main()
         {
-            var customer = new Customer
-            {
-                FirstName = "高橋",
-                LastName = "ジョージ",
-                Email = "example@example.com",
-                BirthDay = new DateTime(2020, 8, 17)
-            };
-            Console.WriteLine($"{customer.FullName} {customer.Email} {customer.BirthDay}");
-
-            var employee = new Employee
-            {
-                FirstName = "小林",
-                LastName = "たかし",
-                Email = "takashi@example.com",
-                BirthDay = new DateTime(2021, 5, 24)
-            };
-            Console.WriteLine($"{employee.FullName} {employee.Email} {employee.BirthDay}");
+            
         }
        
     }
 
-    class Person
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string FullName 
-        {
-            get { return FirstName + LastName; } 
-        }
-        public string Email { get; set; }
-        public DateTime BirthDay { get; set; }
-
-    }
-    
-    class Customer : Person
-    {
-        public string ID { get; set; }
-        public int Rank { get; set; }
-        public int CreditCardNumber { get; set; }
-    }
-
-    class Employee : Person
-    {
-        public int Number { get; set; }
-        public DateTime HireDate { get; set; }
-
-        public override string ToString()
-        {
-            var s = $"{Number} {FullName}" +
-                    $"{HireDate.Year}年{HireDate.Month}月{HireDate.Day}日";
-            return s;
-        }
-    }
 }
