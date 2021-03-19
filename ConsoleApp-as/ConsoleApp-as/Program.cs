ac495fb912d1fd2592001c2566b84aed578bb2a3
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-19 15:03:57 +0900

    overrideを使う　練習

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -9,9 +9,33 @@ namespace DistanceConverter
     {
         static void Main()
         {
-            
+            object book = new Book("人間失格", "太宰治", 450, 4);
+            Console.WriteLine(book);
+            //Console.WriteLine(book.ToString());
         }
        
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public int Rating { get; set; }
+        public Book(string title, string author, int pages, int rating)
+        {
+            Title = title;
+            Author = author;
+            Pages = pages;
+            Rating = rating;
+        }
+
+        public override string ToString()
+        {
+            var s = $"{Title} {Author} {Pages} {Rating}";
+            return s;
+        }
+
     }
 
 }
