ca06255c204970a1614ef7c3a66c8a5ce53e3341
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-20 21:42:00 +0900

    例外が発生しても正しい値が処理を繰り返す　練習

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -9,9 +9,52 @@ namespace DistanceConverter
     {
         static void Main()
         {
-           
+            //One();
+
+            Two();
         }
-        
+
+        private static void One()
+        {
+            while (true)
+            {
+                try
+                {
+                    var total = 1000;
+                    var line = Console.ReadLine();
+                    var count = int.Parse(line);
+                    var ans = total / count;
+                    Console.WriteLine(ans);
+                    Console.WriteLine("正常終了");
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("入力した値が正しくありません。再度入力してください。");
+                }
+            }
+        }
+
+        private static void Two()
+        {
+            while (true)
+            {
+                var total = 1000;
+                var line = Console.ReadLine();
+                if (int.TryParse(line, out var count))
+                {
+                    var ans = total / count;
+                    Console.WriteLine(ans);
+                    Console.WriteLine("seijousyuuryou");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("入力値が間違っています。再度入力してください");
+                }
+                
+            }
+        }
     }
 
 }
