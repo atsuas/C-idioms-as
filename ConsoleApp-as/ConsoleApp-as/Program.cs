1232361f51e03bf53b18896b421edd6b3e6d64db
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-20 21:12:13 +0900

    不要なコードの削除

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -9,30 +9,9 @@ namespace DistanceConverter
     {
         static void Main()
         {
-            //One();
-
-            Two();
+           
         }
-        private static void One()
-        {
-            var array = new int[] { 1, 2, 3, 4, 5 };
-
-            Console.WriteLine(array[5]);
-        }
-
-        private static void Two()
-        {
-            var array = new int[] { 1, 2, 3, 4, 5 };
-
-            try
-            {
-                Console.WriteLine(array[5]);
-            }
-            catch (IndexOutOfRangeException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
+        
     }
 
 }
