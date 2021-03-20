97658b75528af44cd3972bc513bb643131bd1356
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-20 21:59:56 +0900

    不要なコードの削除

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -9,33 +9,7 @@ namespace DistanceConverter
     {
         static void Main()
         {
-            One();
-            Two();
-        }
-
-        private static void One()
-        {
-            var sw = new StreamWriter("sample.txt");
-            try
-            {
-                sw.WriteLine("吾輩は猫である");
-                sw.WriteLine("名前はまだない");
-                sw.WriteLine("どこで生まれたんかとんと見当がつかぬ");
-            }
-            finally
-            {
-                sw.Dispose();
-            }
-        }
-
-        private static void Two()
-        {
-            using (var sw = new StreamWriter("sample2.txt"))
-            {
-                sw.WriteLine("吾輩は猫である");
-                sw.WriteLine("名前はまだない");
-                sw.WriteLine("どこで生まれたんかとんと見当がつかぬ");
-            }
+            
         }
 
     }
