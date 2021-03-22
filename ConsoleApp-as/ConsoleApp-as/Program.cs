f56928a44082d83fedafd6a07a0f8b9368597452
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-22 21:05:02 +0900

    スペースで間をあけて数値を入力する

--- ConsoleApp-as/ConsoleApp-as/Program.cs
+++ ConsoleApp-as/ConsoleApp-as/Program.cs
@@ -9,7 +9,23 @@ namespace DistanceConverter
     {
         static void Main()
         {
-            
+            var line1 = Console.ReadLine();
+            var one = int.Parse(line1);
+            Console.WriteLine();
+
+            int a, b;
+            string[] str = Console.ReadLine().Split(' ');
+            a = int.Parse(str[0]);
+            b = int.Parse(str[1]);
+            var two = a + b;
+            Console.WriteLine();
+
+            var four = Console.ReadLine();
+
+            var total = one + two;
+
+            Console.WriteLine($"{total} {four}");
+
         }
 
     }
